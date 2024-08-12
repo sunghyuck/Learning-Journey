80339a6841d905a943212a81442d0a9894ee66c9
HongSungHyuck <jhk139814@gmail.com>
2024-08-12 23:25:56 +0900

    test

--- journey/src/main/java/com/learningjourney/journey/JourneyApplication.java
+++ journey/src/main/java/com/learningjourney/journey/JourneyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JourneyApplication {
 
 	public static void main(String[] args) {
+		System.out.println();
 		SpringApplication.run(JourneyApplication.class, args);
 	}
 
