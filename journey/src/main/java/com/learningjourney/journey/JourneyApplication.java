04dfac9516aa78d7902f5c60abe0e7fdc89373e1
HongSungHyuck <jhk139814@gmail.com>
2024-08-12 23:22:12 +0900

    Initial commit with Spring Boot project

---
+++ journey/src/main/java/com/learningjourney/journey/JourneyApplication.java
@@ -0,0 +1,13 @@
+package com.learningjourney.journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyApplication.class, args);
+	}
+
+}
---
+++ journey/src/test/java/com/learningjourney/journey/JourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningjourney.journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
